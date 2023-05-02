4e6650662e6df86a15dafe76335ede69b447b292
Matías <mjlescano@protonmail.com>
2023-05-02 17:22:32 +0200

    parallelize read deploys

--- packages/builder/src/package.ts
+++ packages/builder/src/package.ts
@@ -31,19 +31,54 @@ export async function forPackageTree<T>(
 ): Promise<T[]> {
   const results: T[] = [];
 
-  for (const stepState of Object.entries(deployInfo.state || {})) {
-    for (const importArtifact of Object.entries((stepState[1] as StepState).artifacts.imports || {})) {
-      if (!onlyProvisioned || importArtifact[1].tags) {
-        const nestedDeployInfo = await store.readBlob(importArtifact[1].url);
-        const result = await forPackageTree(store, nestedDeployInfo, action, importArtifact[1], onlyProvisioned);
-        results.push(...result);
-      }
-    }
+  const deployments = await readDeployRecursive(store, deployInfo, onlyProvisioned);
+
+  for (const [nestedOutput, nestedDeployInfo] of deployments) {
+    const result = await forPackageTree(store, nestedDeployInfo, action, nestedOutput, onlyProvisioned);
+    results.push(...result);
   }
 
   results.push(await action(deployInfo, context || null));
 
   return results;
+}
+
+/**
+ * Get the imported/provisioned dependencies from the given deployment, ordered Depth-First-Search.
+ * @param loader The loader to use for downloading sub-packages
+ * @param deployInfo The head node of the tree, which will be executed on `action` last
+ * @param onlyProvisioned Skip over sub-packages which are not provisioned within the parent
+ */
+export async function readDeployRecursive(store: CannonStorage, deployInfo: DeploymentInfo, onlyProvisioned = true) {
+  const result = new Map<string, [BundledOutput, DeploymentInfo]>();
+
+  const _readImports = async (deployInfo: DeploymentInfo) => {
+    const bundledOutputs = _getImports(deployInfo);
+
+    // Recursively get the imports of the loaded deployments, in Depth-First-Search order
+    // TODO: parallelize the requests, we are doing it like this to be able to keep the tree order.
+    for (const output of bundledOutputs) {
+      // Check that the dependency is not already loaded to avoid download loops.
+      if (result.has(output.url)) continue;
+      if (onlyProvisioned && !output.tags) continue;
+
+      const misc = (await store.readBlob(output.url)) as DeploymentInfo;
+      if (!misc) throw new Error(`deployment not found: ${output.url}`);
+
+      await _readImports(misc);
+      result.set(output.url, [output, misc]);
+    }
+  };
+
+  await _readImports(deployInfo);
+
+  return Array.from(result.values());
+}
+
+// Get the urls of the imported packages
+function _getImports(deployInfo: DeploymentInfo) {
+  if (!deployInfo.state) return [];
+  return Object.values(deployInfo.state).flatMap((state) => Object.values(state.artifacts.imports || {}));
 }
 
 export async function copyPackage({ packageRef, tags, variant, fromStorage, toStorage, recursive }: CopyPackageOpts) {
