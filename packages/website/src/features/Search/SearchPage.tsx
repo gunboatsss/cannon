d93261ff30d326aa1bca7176c408e0be2bb8d491
Amin Latifi <a.latifi.al@gmail.com>
2023-07-20 12:38:36 +0330

    Fixed some components of SearchPage

--- packages/website/src/features/Search/SearchPage.tsx
+++ packages/website/src/features/Search/SearchPage.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Grid, GridItem, Input } from '@chakra-ui/react';
+import {
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
 import { useQuery } from '@apollo/client';
 import { GET_PACKAGES, TOTAL_PACKAGES } from '@/graphql/queries';
 import {
@@ -11,6 +18,8 @@ import {
   GetTotalPackagesQueryVariables,
 } from '@/types/graphql/graphql';
 import { PackagePreview } from '@/features/Search/PackagePreview';
+import { scale } from '@chakra-ui/tooltip/dist/tooltip.transition';
+import { Search } from 'react-feather';
 
 export const SearchPage = () => {
   const [page, setPage] = useState<number>(1);
@@ -65,12 +74,46 @@ export const SearchPage = () => {
   }, [totalPackages]);
 
   return (
-    <Grid gap={6}>
-      <GridItem>
-        <div>
+    <Grid
+      template-columns="repeat(12, 1fr)"
+      gap="6"
+      py="10"
+      maxWidth="containers.lg"
+      mx="auto"
+      px="4"
+      // spacing="40px" TODO: not found equivalent in react
+    >
+      <GridItem colSpan={[12, 3]}>
+        <Heading
+          as="h3"
+          size="sm"
+          textTransform="uppercase"
+          fontWeight="normal"
+          letterSpacing="1px"
+          mb="2"
+        >
           Search
-          <Input onChange={handleSearch} />
-        </div>
+        </Heading>
+        <InputGroup>
+          <InputLeftElement>
+            <Search
+              style={{
+                transform: 'scale(0.8)',
+                transformOrigin: 'center left',
+                opacity: 0.66,
+              }}
+
+              // v-html="$feathericons['search'].toSvg()"
+            />
+          </InputLeftElement>
+          <Input
+            background="transparent"
+            size="sm"
+            borderColor="gray.500"
+            mb="6"
+            onChange={handleSearch}
+          />
+        </InputGroup>
         <div>
           Showing page {page} of {totalPages}
         </div>
